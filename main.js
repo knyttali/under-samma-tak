07aeb5773968a2dabaf2fbf40a6ea628658bb83f
AndreasSjo <andreas.sjoden@hotmail.com>
2022-04-13 13:27:54 +0200

    fix slidecounter

--- main.js
+++ main.js
@@ -3,14 +3,18 @@
 var personligInfo = document.getElementById("personligInfo");
 var guestWhere = document.getElementById("guestWhereInfo");
 var guests = document.getElementById("guestInfo");
+var length = document.getElementById("lengthInfo");
+var send = document.getElementById("sendInfo");
 //add rest of slides
 
-var slideCounter =0;
+var slideCounter = 0;
 
 document.getElementById("next").addEventListener("click", nextForm)
 
 function nextForm() {
+
     slideCounter = slideCounter + 1; 
+
     if (slideCounter == 1) {
         personligInfo.style.display="none"
         guestWhere.style.display="block";//Vet inte om denna är det vi vill ha men måste använda nått
@@ -21,9 +25,12 @@ function nextForm() {
     }
     else if (slideCounter == 3) {
         guests.style.display="none"
+        length.style.display="block"
         // I förts skede sliden style.display="block"
     }
     else if (slideCounter == 4) {
+        length.style.display="none"
+        send.style.display="block"
         // I förts skede sliden style.display="none"
         //Kontrolera uppgifter style.display="block"
     }
