4d9a78c7e842ccdd6280dd9476194c1f0e90a973
knyttali <amanda_andrae@hotmail.com>
2022-05-03 13:55:12 +0200

    nya loggan är utanför headern

--- components/Header.js
+++ components/Header.js
@@ -1,33 +1,28 @@
-import Image from "next/image"
-import logo from "../public/Images/Logo_blue.png"
-import globe from "../public/Images/Vector.png"
-import Link from "next/dist/client/link"
+import Image from "next/image";
+import logo from "../public/Images/Logo_blue.png";
+import globe from "../public/Images/Globe.png";
+import Link from "next/dist/client/link";
+import Logo from "../public/Images/Logo.svg";
 
 const Header = () => {
   return (
     <div id="header" className="d-flex align-items-center">
+      
 
-    <div id="logoDiv">
-      <Link href ="/">
-        <a>
-            <Image src={logo}>
-            </Image>
-        </a>
+        <Link href="/">
+          <a>
+            <Image src={Logo} className="logoimg"></Image>
+          </a>
         </Link>
-    </div>
 
-    <div id="languageIconDiv" className="d-flex align-items-center">
-      <div id = "globe">
-        <Image src={globe}></Image>
+
+      <div id="languageIconDiv" className="d-flex align-items-center">
+        <div id="globe">
+          <Image src={globe}></Image>
         </div>
-        <div className="language" id="languageText">
-        <p>
-            Language
-        </p>
-        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Header;
