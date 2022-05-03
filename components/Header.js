8922ba45ac79484679ad2b20e1033eeb55d1ec42
knyttali <amanda_andrae@hotmail.com>
2022-05-03 15:50:22 +0200

    headern fast ful

--- components/Header.js
+++ components/Header.js
@@ -3,20 +3,36 @@ import logo from "../public/Images/Logo_blue.png";
 import globe from "../public/Images/Globe.png";
 import Link from "next/dist/client/link";
 import Logo from "../public/Images/Logo.svg";
+import search from "../public/Images/Search.svg";
 
 const Header = () => {
   return (
     <div id="header" className="d-flex align-items-center">
-      
-
+      <div id="logoimg">
         <Link href="/">
           <a>
-            <Image src={Logo} className="logoimg"></Image>
+            <Image src={Logo}></Image>
           </a>
         </Link>
+      </div>
 
+      <div className="menuItems justify-content-between d-flex">
+        <Link href="/">
+          <a>Så fungerar det</a>
+        </Link>
+        <Link href="/">
+          <a>Vårt uppdrag</a>
+        </Link>
+        <Link href="/">
+          <a>Vanliga frågor</a>
+        </Link>
+        <Link href="/FormPage">
+          <a>Anmäl ditt intresse</a>
+        </Link>
+      </div>
 
       <div id="languageIconDiv" className="d-flex align-items-center">
+        <Image src={search}></Image>
         <div id="globe">
           <Image src={globe}></Image>
         </div>
