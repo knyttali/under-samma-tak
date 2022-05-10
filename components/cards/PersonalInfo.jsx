72073b3ad1844b44cd0e07f751df5e77bee5c7ca
knyttali <amanda_andrae@hotmail.com>
2022-05-10 10:42:49 +0200

    går att skriva i första kortet, tog bort value

--- components/cards/PersonalInfo.jsx
+++ components/cards/PersonalInfo.jsx
@@ -1,82 +1,123 @@
-import React from 'react'
-import NextButton from '../NextButton'
+import React from "react";
+import NextButton from "../NextButton";
 const PersonalInfo = () => {
   return (
     <>
-    <h4 className="form-header ">Personlig information</h4>
-    
+      <h4 className="form-header ">Personlig information</h4>
 
-    <div className="name-row row">
-      <div className="col-6">
-        <label htmlFor="fname">Förnamn</label><br />
-        <input required type="text" id="fname" name="fname" value="" className="w-100"/>
+      <div className="name-row row">
+        <div className="col-6">
+          <label htmlFor="fname">Förnamn</label>
+          <br />
+          <input
+            required
+            type="text"
+            id="fname"
+            name="fname"
+            className="w-100"
+          />
+        </div>
+
+        <div className="col-6">
+          <label htmlFor="lname">Efternamn</label>
+          <br />
+          <input
+            required
+            type="text"
+            id="lname"
+            name="lname"
+            className="w-100"
+          />
+        </div>
       </div>
 
-      <div className="col-6">
-        <label htmlFor="lname">Efternamn</label><br />
-        <input required type="text" id="lname" name="lname" value="" className="w-100"/>
-      </div>
-    </div>
-
-    <div className="adress-row row">
-      <div className="col-12">
-        <label htmlFor="address">Gatuadress</label><br />
-        <input required
-          type="text"
-          id="address"
-          name="address"
-          value=""
-          className="w-100"
-          // onChange={} Nödvändig men vet inte vad den gör, behövs på alla inputs
-        />
-      </div>
-    </div>
-
-    <div className="kommun-row row">
-      <div className="col-6">
-        <label htmlFor="ort">Ort</label><br />
-        <input required type="text" id="ort" name="ort" value="" className="w-100" />
+      <div className="adress-row row">
+        <div className="col-12">
+          <label htmlFor="address">Gatuadress</label>
+          <br />
+          <input
+            required
+            type="text"
+            id="address"
+            name="address"
+            className="w-100"
+            // onChange={} Nödvändig men vet inte vad den gör, behövs på alla inputs
+          />
+        </div>
       </div>
 
-      <div className="col-6">
-        <label htmlFor="kommun">Kommun</label><br />
-        <input required
-          id="autoComplete"
-          type="text"
-          dir="ltr"
-          spellCheck="false"
-          autoCorrect="off"
-          autoComplete="off"
-          autoCapitalize="off"
-          className="w-100"
-        />
+      <div className="kommun-row row">
+        <div className="col-6">
+          <label htmlFor="ort">Ort</label>
+          <br />
+          <input
+            required
+            type="text"
+            id="ort"
+            name="ort"
+            className="w-100"
+          />
+        </div>
+
+        <div className="col-6">
+          <label htmlFor="kommun">Kommun</label>
+          <br />
+          <input
+            required
+            id="autoComplete"
+            type="text"
+            dir="ltr"
+            spellCheck="false"
+            autoCorrect="off"
+            autoComplete="off"
+            autoCapitalize="off"
+            className="w-100"
+          />
+        </div>
       </div>
-    </div>
-    <div className="nummer-row row">
-      <div className="col-6">
-        <label htmlFor="postNum">Postnummer</label><br />
-        <input required type="text" id="postNum" name="postNum" value="" className="w-100"/>
+      <div className="nummer-row row">
+        <div className="col-6">
+          <label htmlFor="postNum">Postnummer</label>
+          <br />
+          <input
+            required
+            type="text"
+            id="postNum"
+            name="postNum"
+            className="w-100"
+          />
+        </div>
+
+        <div className="col-6">
+          <label htmlFor="phone">Telefonnummer</label>
+          <br />
+          <input
+            required
+            type="text"
+            id="phone"
+            name="phone"
+            className="w-100"
+          />
+        </div>
       </div>
 
-      <div className="col-6">
-        <label htmlFor="phone">Telefonnummer</label><br />
-        <input required type="text" id="phone" name="phone" value="" className="w-100"/>
+      <div className="email-row row">
+        <div className="col-6">
+          <label htmlFor="email">E-mail adress</label>
+          <br />
+          <input
+            required
+            type="email"
+            id="email"
+            name="email"
+            className="w-100"
+          />
+        </div>
+
+        <div className="col-6"></div>
       </div>
-    </div>
+    </>
+  );
+};
 
-    <div className="email-row row">
-      <div className="col-6">
-        <label htmlFor="email">E-mail adress</label><br />
-        <input required type="email" id="email" name="email" value="" className="w-100"/>
-      </div>
-
-      <div className="col-6"></div>
-
-    </div>
-    
-   </>
-  
-  )
-}
-
-export default PersonalInfo
+export default PersonalInfo;
