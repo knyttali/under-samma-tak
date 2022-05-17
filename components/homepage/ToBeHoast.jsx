e96a7b03dc2eaefce1cbddebe951caa532760734
knyttali <amanda_andrae@hotmail.com>
2022-05-17 12:14:26 +0200

    FAQ

--- components/homepage/ToBeHoast.jsx
+++ components/homepage/ToBeHoast.jsx
@@ -2,15 +2,20 @@ import IntroButton from "../IntroButton"
 
 function ToBeHoast() {
   return (
-    <div className="toBeHoast-container">
-        <h6 className="col-2 homepageH6">ATT VARA VÄRDFAMILJ</h6>
-        <h2 className="col-6">Du hjälper utifrån dina villkor</h2>
-        <div className="col-4 listan">
+    <div className="sameSides">
+        <div className="sida">sida</div>
+        <div className="toBeHoastContent">
+          <h6 className="homepageH6">ATT VARA VÄRDFAMILJ</h6>
+        <h2 className="">Du hjälper utifrån dina villkor</h2>
+        <div className="listan">
             <p>1. Anmäl ditt intresse genom att fylla i dina villkor</p>
             <p>2. Få en bekräftelse på att dina uppgifter är sparade</p>
             <p>3. Invänta eventuell återkoppling från din kommun</p>
         </div>
         <button className="btn-primary">Så fungerar det</button>
+        </div>
+        
+        <div className="sida">sida</div>
     </div>
   )
 }
