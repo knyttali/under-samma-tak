179c75c025365ab362925a583e7ae639259cb47f
knyttali <amanda_andrae@hotmail.com>
2022-05-18 10:17:35 +0200

    bakgrundbild och knappfärg

--- components/cards/PersonalInfo.jsx
+++ components/cards/PersonalInfo.jsx
@@ -12,16 +12,16 @@ export class PersonalInfo extends Component {
       <form className="personal-info form-template col-6" id="personligInfo">
        <h4 className="form-header ">Personlig information</h4>
 
-<div className="name-row row">
-  <div className="col-6">
+<div className="name-row placement-inputs">
+  <div className="placement">
     <label htmlFor="fname">Förnamn</label><br />
-    <input required type="text" id="fname" name="fname" defaultValue={values.fname} onChange={handleChange('fname')} className="w-100"/>
+    <input required type="text" id="fname" name="fname" defaultValue={values.fname} onChange={handleChange('fname')} />
     <p className='errorRed'>{values.validFname}</p>
   </div>
 
-  <div className="col-6">
+  <div className="placement">
     <label htmlFor="lname">Efternamn</label><br />
-    <input required type="text" id="lname" name="lname" defaultValue={values.lname} onChange={handleChange('lname')} className="w-100"/>
+    <input required type="text" id="lname" name="lname" defaultValue={values.lname} onChange={handleChange('lname')} />
     <p className='errorRed'>{values.validLname}</p>
   </div>
 </div>
@@ -41,44 +41,44 @@ export class PersonalInfo extends Component {
   </div>
 </div>
 
-<div className="kommun-row row">
-  <div className="col-6">
+<div className="kommun-row placement-inputs">
+  <div className="placement">
     <label htmlFor="ort">Ort</label><br />
-    <input required type="text" id="ort" name="ort" defaultValue={values.ort} onChange={handleChange('ort')} className="w-100" />
+    <input required type="text" id="ort" name="ort" defaultValue={values.ort} onChange={handleChange('ort')}  />
     <p className='errorRed'>{values.validOrt}</p>
   </div>
 
-  <div className="col-6">
+  <div className="placement">
     <label htmlFor="kommun">Kommun</label><br />
     <input required
       id="kommun"
       type="text"
       defaultValue={values.kommun}
       onChange={handleChange('kommun')}
-      className="w-100"
+      
     />
     <p className='errorRed'>{values.validKommun}</p>
   </div>
 </div>
-<div className="nummer-row row">
-  <div className="col-6">
+<div className="nummer-row placement-inputs">
+  <div className="placement">
     <label htmlFor="postNum">Postnummer</label><br />
-    <input required type="text" id="postNum" name="postNum" defaultValue={values.postNum} onChange={handleChange('postNum')} className="w-100"/>
+    <input required type="text" id="postNum" name="postNum" defaultValue={values.postNum} onChange={handleChange('postNum')} />
   </div>
 
-  <div className="col-6">
+  <div className="placement">
     <label htmlFor="phone">Telefonnummer</label><br />
-    <input required type="text" id="phone" name="phone" defaultValue={values.phone} onChange={handleChange('phone')} className="w-100"/>
+    <input required type="text" id="phone" name="phone" defaultValue={values.phone} onChange={handleChange('phone')} />
   </div>
 </div>
 
-<div className="email-row row">
-  <div className="col-6">
+<div className="email-row placement-inputs">
+  <div className="placement">
     <label htmlFor="email">E-mail adress</label><br />
-    <input required type="email" id="email" name="email" defaultValue={values.email} onChange={handleChange('email')} className="w-100"/>
+    <input required type="email" id="email" name="email" defaultValue={values.email} onChange={handleChange('email')} />
   </div>
 
-  <div className="col-6"></div>
+  <div className="placement-right"></div>
 
 </div>
 <button type="button" className="buttonNext" onClick={this.continue}>
