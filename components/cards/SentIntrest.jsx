6e1e2f4f937237c739060c60361e3a82b9f94deb
Filip Mathsson <pigfof@gmail.com>
2022-05-19 11:20:16 +0200

    Fixed SentIntrest to new design + added image

--- components/cards/SentIntrest.jsx
+++ components/cards/SentIntrest.jsx
@@ -1,23 +1,42 @@
 import React, { Component } from 'react'
 import Link from "next/dist/client/link";
-
+import Skelleftea from "../../public/Images/skellefteaKommun.svg"
+import Image from "next/image";
 export class SentIntrest extends Component {
   
   render() {
     const { values, handleChange } = this.props;
     return (
+      <div className='main-container row'>
+        <div className="col-3"></div>
       <form className="length-info form-template col-6" id="sentIntrest" >
       <h1 className="form-header">
-        Intresseanmälan Skickad
+        Intresseanmälan skickad
       </h1>
-      <p>Tack! Din intresseanmälan är nu skickad till din kommun! Vid eventuella frågor, kontakta oss eller din kommun.</p>
+      <p>Tack för att du är intresserad av att hjälpa till!</p>
+      <br /><br />
+      <p style={{fontWeight: "Bold"}}>Vad händer nu?</p>
+      <p>
+        1. Din kommun kommer att se över din intresseanmälan. <br />
+        2. Din kommun bjuder in dig till en orienterande träff med berörda parter. <br />
+        3. Du väljer själv om du vill gå vidate och bli en bärdfamilj
+      </p>
+      <br />
+      <br />
+      <p>Vid eventuella frågor, <a href=''>kontakta oss</a> eller din kommun.</p>
+      <br />
+      <a href="">Skellefteå kommun</a>
+      <br />
+      <Image src={Skelleftea}></Image>
+      <br />
       <Link href={"/"}>
            <button type="button" className="buttonNext">
-              ok
+              Ok
           </button>
           </Link>
      
     </form>
+    </div>
     )
   }
 }
