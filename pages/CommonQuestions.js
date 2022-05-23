ba7c4ad4939385ddd1a84cc3fdcdb083efbceaf5
denisecasp <den.casperson@gmail.com>
2022-05-23 16:21:01 +0200

    Mer info + tillbaka

--- pages/CommonQuestions.js
+++ pages/CommonQuestions.js
@@ -214,58 +214,160 @@ const CommonQuestions = () => {
     function homePage(){
       //HOME PAGE
         setQna(
-            <div className="faq-div">
-        <Header />
-  
-        <div
-          style={{
-            backgroundImage: 'url("https://i.imgur.com/tTpxOdk.png%22)',
-            backgroundRepeat: "none",
-            backgroundSize: "cover",
-          }}
-        >
-          <div className="introContainer d-flex align-items-center">
-            <div className="our-mission-title">
-              <h1>Vanliga frågor</h1>
+          <div className="faq-div">
+          <Header />
+    
+          <div
+            style={{
+              backgroundImage: 'url("https://i.imgur.com/ZqJ3pYj.png")',
+              backgroundRepeat: "none",
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="introContainer d-flex align-items-center">
+              <div className="our-mission-title">
+                <h1>Vanliga frågor</h1>
+              </div>
             </div>
           </div>
-        </div>
-  
-        <div className="faq-row1 row">
-          <div className="col-1" />
-          <div className="col-4">
-            <p className="faq-row-header">
-              Kan jag välja att avsluta när jag vill?
-            </p>
-            <p className="faq-p">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Asperiores, reprehenderit? Hic optio voluptatem odit? Quia velit
-              voluptatem amet commodi optio officia deleniti, culpa provident,
-              accusamus voluptatum pariatur asperiores facilis perferendis?
-            </p>
-            <button onClick={readMoreQ1}>ReadMore</button>
+    
+          <div className="faq-row1 row">
+            <div className="col-1" />
+            <div className="col-4">
+              <p className="faq-row-header">
+                Kan jag välja att avsluta när jag vill?
+              </p>
+              <p className="faq-p">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Asperiores, reprehenderit? Hic optio voluptatem odit? Quia velit
+                voluptatem amet commodi optio officia deleniti, culpa provident,
+                accusamus voluptatum pariatur asperiores facilis perferendis?<p className="ReadMore" onClick={readMoreQ1}> Läs mer</p>
+              </p>
+            </div>
+            <div className="col-1" />
+            <div className="col-4">
+              <p className="faq-row-header">
+              Är de som kommer försäkrade?
+              </p>
+              <p className="faq-p">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Asperiores, reprehenderit? Hic optio voluptatem odit? Quia velit
+                voluptatem amet commodi optio officia deleniti, culpa provident,
+                accusamus voluptatum pariatur asperiores facilis perferendis?<p className="ReadMore" onClick={readMoreQ1}> Läs mer</p>
+              </p>
+            </div>
+            <div className="col-2" />
           </div>
-          <div className="col-1" />
-          <div className="col-4">
-            <p className="faq-row-header">
-            Är de som kommer försäkrade?
-            </p>
-            <p className="faq-p">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Asperiores, reprehenderit? Hic optio voluptatem odit? Quia velit
-              voluptatem amet commodi optio officia deleniti, culpa provident,
-              accusamus voluptatum pariatur asperiores facilis perferendis?
-            </p>
+    
+          <div className="faq-row2 row"> <div className="col-1" />
+            <div className="col-4">
+              <p className="faq-row-header">
+              Vad händer med flyktingarna sen?
+              </p>
+              <p className="faq-p">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Asperiores, reprehenderit? Hic optio voluptatem odit? Quia velit
+                voluptatem amet commodi optio officia deleniti, culpa provident,
+                accusamus voluptatum pariatur asperiores facilis perferendis?<p className="ReadMore" onClick={readMoreQ1}> Läs mer</p>
+              </p>
+              
+            </div>
+            <div className="col-1" />
+            <div className="col-4">
+              <p className="faq-row-header">
+              Gäller min hemförsäkring?
+              </p>
+              <p className="faq-p">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Asperiores, reprehenderit? Hic optio voluptatem odit? Quia velit
+                voluptatem amet commodi optio officia deleniti, culpa provident,
+                accusamus voluptatum pariatur asperiores facilis perferendis?<p className="ReadMore" onClick={readMoreQ1}> Läs mer</p>
+              </p>
+            </div>
+            <div className="col-2" />
           </div>
-          <div className="col-2" />
+          <div className="faq-row3 row"> <div className="col-1" />
+            <div className="col-4">
+              <p className="faq-row-header">
+              Får man ersättning?
+              </p>
+              <p className="faq-p">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Asperiores, reprehenderit? Hic optio voluptatem odit? Quia velit
+                voluptatem amet commodi optio officia deleniti, culpa provident,
+                accusamus voluptatum pariatur asperiores facilis perferendis?<p className="ReadMore" onClick={readMoreQ1}> Läs mer</p>
+              </p>
+             
+            </div>
+            <div className="col-1" />
+            <div className="col-4">
+              <p className="faq-row-header">
+              Kan man välja vilka som kommer?
+              </p>
+              <p className="faq-p">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Asperiores, reprehenderit? Hic optio voluptatem odit? Quia velit
+                voluptatem amet commodi optio officia deleniti, culpa provident,
+                accusamus voluptatum pariatur asperiores facilis perferendis?<p className="ReadMore" onClick={readMoreQ1}> Läs mer</p>
+              </p>
+            </div>
+            <div className="col-2" />
+          </div>
+          <div className="faq-row4 row"> <div className="col-1" />
+            <div className="col-4">
+              <p className="faq-row-header">
+              Vem betalar för vad?
+              </p>
+              <p className="faq-p">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Asperiores, reprehenderit? Hic optio voluptatem odit? Quia velit
+                voluptatem amet commodi optio officia deleniti, culpa provident,
+                accusamus voluptatum pariatur asperiores facilis perferendis?<p className="ReadMore" onClick={readMoreQ1}> Läs mer</p>
+              </p>
+              
+            </div>
+            <div className="col-1" />
+            <div className="col-4">
+              <p className="faq-row-header">
+              Vilka kontroller görs?
+              </p>
+              <p className="faq-p">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Asperiores, reprehenderit? Hic optio voluptatem odit? Quia velit
+                voluptatem amet commodi optio officia deleniti, culpa provident,
+                accusamus voluptatum pariatur asperiores facilis perferendis?<p className="ReadMore" onClick={readMoreQ1}> Läs mer</p>
+              </p>
+            </div>
+            <div className="col-2" />
+          </div>
+          <div className="faq-row5 row"> <div className="col-1" />
+            <div className="col-4">
+              <p className="faq-row-header">
+              Får de som kommer annat stöd?
+              </p>
+              <p className="faq-p">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Asperiores, reprehenderit? Hic optio voluptatem odit? Quia velit
+                voluptatem amet commodi optio officia deleniti, culpa provident,
+                accusamus voluptatum pariatur asperiores facilis perferendis?<p className="ReadMore" onClick={readMoreQ1}> Läs mer</p>
+              </p>
+            </div>
+            <div className="col-1" />
+            <div className="col-4">
+              <p className="faq-row-header">
+              När kommer de?
+              </p>
+              <p className="faq-p">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Asperiores, reprehenderit? Hic optio voluptatem odit? Quia velit
+                voluptatem amet commodi optio officia deleniti, culpa provident,
+                accusamus voluptatum pariatur asperiores facilis perferendis?<p className="ReadMore" onClick={readMoreQ1}> Läs mer</p>
+              </p>
+            </div>
+            <div className="col-2" />
+          </div>
+          <Footer />
         </div>
-  
-        <div className="faq-row2"></div>
-        <div className="faq-row3"></div>
-        <div className="faq-row4"></div>
-        <div className="faq-row5"></div>
-        <Footer />
-      </div>
         )
     }
     
